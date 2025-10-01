8767153862138d956411a496f65ca59c229abac1
maariopaalma <maariopaalma@gmail.com>
2025-10-01 18:27:58 +0200

    Mapa añadido

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     mensaje.innerText = "Hola Nathan Drake";
   });
 });
+
+// Función que inicializa el mapa
+function iniciarMap() {
+  const centro = { lat: 40.4168, lng: -3.7038 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: centro,
+    zoom: 12,
+    mapId: "DEMO_MAP_ID" // opcional, para usar estilos avanzados
+  });
+
+  // Nuevo marcador recomendado (AdvancedMarkerElement)
+  new google.maps.marker.AdvancedMarkerElement({
+    map: map,
+    position: centro,
+    title: "Madrid Centro"
+  });
+}
